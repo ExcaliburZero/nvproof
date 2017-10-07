21bda52ea0fb19d1856433c3a9e388ea9a189543
Christopher Wells <cwellsny@nycap.rr.com>
2017-10-07 15:15:18 -0400

    Implement proof file parsing

--- src/main/scala/nvproof/AST.scala
+++ src/main/scala/nvproof/AST.scala
@@ -1,23 +1,45 @@
 package nvproof
 
-case class Step(lineNumber: AST.LineNumber, statement: Statement, rule: Rule)
+case class Step(lineNumber: AST.LineNumber, statement: Statement, rule: Rule) {
+  override def toString(): String = f"$lineNumber) $statement $rule"
+}
+
 
 sealed trait Statement
-case class UnaryExpression(unaryOperator: UnaryOperator, statement: Statement) extends Statement
-case class BinaryExpression(statement1: Statement, binaryOperator: BinaryOperator, statement2: Statement) extends Statement
-case class Symbol(char: Char) extends Statement
+case class UnaryExpression(unaryOperator: UnaryOperator, statement: Statement) extends Statement {
+  override def toString(): String = f"$unaryOperator$statement"
+}
+case class BinaryExpression(statement1: Statement, binaryOperator: BinaryOperator, statement2: Statement) extends Statement {
+  override def toString(): String = f"($statement1 $binaryOperator $statement2)"
+}
+
+case class Symbol(char: Char) extends Statement {
+  override def toString(): String = char.toString
+}
 
 sealed trait UnaryOperator
-case class Not() extends UnaryOperator
+case class Not() extends UnaryOperator {
+  override def toString(): String = "~"
+}
+
 case class Necessary() extends UnaryOperator
 case class Possible() extends UnaryOperator
 
 sealed trait BinaryOperator
-case class Implication() extends BinaryOperator
+case class Implication() extends BinaryOperator {
+  override def toString(): String = "⇒"
+}
+
 
 sealed trait Rule
-case class Assumption() extends Rule
-case class ModusPonens(lineNumber1: AST.LineNumber, lineNumber2: AST.LineNumber) extends Rule
+case class Assumption() extends Rule {
+  override def toString(): String = "AS"
+}
+
+case class ModusPonens(lineNumber1: AST.LineNumber, lineNumber2: AST.LineNumber) extends Rule {
+  override def toString(): String = f"MP $lineNumber1, $lineNumber2"
+}
+
 
 object AST {
   type Proof = List[Step]
--- src/main/scala/nvproof/Main.scala
+++ src/main/scala/nvproof/Main.scala
@@ -1,5 +1,22 @@
 package nvproof
 
-object Main extends App {
-  println("Hello, World!")
+object Main {
+  def main(argv: Array[String]): Unit = {
+    print("> ")
+    val fileName = readLine()
+    val fileContents = readFile(fileName)
+
+    val proof = Parsers.parse(Parsers.proof, fileContents)
+
+    proof match {
+      case Parsers.Success(matched,_) => for (ln <- matched) yield println(ln)
+      case Parsers.Failure(msg,_) => println("FAILURE: " + msg)
+      case Parsers.Error(msg,_) => println("ERROR: " + msg)
+    }
+  }
+
+  def readFile(fileName: String): String = {
+    val source = scala.io.Source.fromFile(fileName)
+    try source.mkString finally source.close()
+  }
 }
