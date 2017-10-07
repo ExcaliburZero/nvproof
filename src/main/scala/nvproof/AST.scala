7b02d5efc336590b0bc8549cfd989a488809ec4f
Christopher Wells <cwellsny@nycap.rr.com>
2017-10-07 13:12:10 -0400

    Start defining AST

---
+++ src/main/scala/nvproof/AST.scala
@@ -0,0 +1,26 @@
+package nvproof
+
+import scala.util.parsing.combinator._
+
+case class Step(lineNumber: AST.LineNumber, statement: Statement, rule: Rule)
+
+sealed trait Statement
+case class UnaryExpression(unaryOperator: UnaryOperator, statement: Statement) extends Statement
+case class BinaryExpression(statement1: Statement, binaryOperator: BinaryOperator, statement2: Statement) extends Statement
+case class Symbol(char: Char) extends Statement
+
+sealed trait UnaryOperator
+case class Not() extends UnaryOperator
+case class Necessary() extends UnaryOperator
+case class Possible() extends UnaryOperator
+
+sealed trait BinaryOperator
+case class Implication() extends BinaryOperator
+
+sealed trait Rule
+case class Assumption() extends Rule
+case class ModusPonens(lineNumber1: AST.LineNumber, lineNumber2: AST.LineNumber) extends Rule
+
+object AST {
+  type LineNumber = Int
+}
---
+++ src/test/scala/ASTTests.scala
@@ -0,0 +1,16 @@
+import org.scalatest.{Assertion, FlatSpec, Matchers}
+
+import nvproof._
+
+import scala.util.parsing.combinator._
+
+class ASTTests extends FlatSpec with Matchers {
+  "AST" should "allow one to represent a simple proof" in {
+    List(
+        Step(1, BinaryExpression(Symbol('P'), Implication(), Symbol('Q')), Assumption())
+      , Step(2, UnaryExpression(Not(), Symbol('Q')), ModusPonens(1, 2))
+    )
+
+    1 shouldBe 1
+  }
+}
